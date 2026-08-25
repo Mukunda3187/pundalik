5f6438a1311ae767e5cf3a825500bc9318bd844a
Naradasu Mukunda Rao <cbit051@gmail.com>
2026-08-25 11:29:35 +0530

    Rename buildPfile function to buildProfile

--- backend/src/astro/chartBuilder.js
+++ backend/src/astro/chartBuilder.js
@@ -14,7 +14,7 @@ const { computeNumerology } = require('./numerology');
  * @param {number} input.longitude
  * @param {string} input.placeName
  */
-function buildPfile(input) {
+function buildProfile(input) {
   const { name, day, month, year, hour, minute, tzOffsetHours, latitude, longitude, placeName } = input;
 
   // Convert local civil time to UTC
